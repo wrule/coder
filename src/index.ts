534566ee415b0f03c6d9aff35c4ae7981b9d4738
wrule <1982775886@qq.com>
2026-02-13 22:55:11 +0800

    feat: main

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import 'dotenv/config';
+import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+import { streamText } from 'ai';
 
-function main() {
-  console.log(process.env.OPENROUTER_API_KEY);
+async function main() {
+  const openrouter = createOpenRouter({
+    apiKey: process.env.OPENROUTER_API_KEY!,
+  });
+  const result = streamText({
+    model: openrouter.chat('meta-llama/llama-3.1-405b-instruct'),
+    prompt: 'Write a short story about AI.',
+  });
+
+  for await (const chunk of result.textStream) {
+    console.log(chunk);
+  }
 }
 
 main();
