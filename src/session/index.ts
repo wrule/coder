657f4dd2c3a4ab0ff86aa690c4aace53246c807d
wrule <1982775886@qq.com>
2026-02-14 18:48:43 +0800

    feat: loop chat

--- src/session/index.ts
+++ src/session/index.ts
@@ -1,5 +1,6 @@
 import { streamText, type LanguageModel, type UserModelMessage } from 'ai';
 import Message from './message';
+import dayjs from 'dayjs';
 
 class Session {
   public constructor(
@@ -16,18 +17,25 @@ class Session {
   }
 
   public async SendTextMessage(content: string) {
-    const newMessage: UserModelMessage = {
-      role: 'user',
-      content,
-    };
-    const newModelMessages = this.ModelMessages.concat(newMessage);
+    const newMessage: UserModelMessage = { role: 'user', content };
+    const questionSendTime = dayjs();
+    this.messages.push(new Message(newMessage, questionSendTime, questionSendTime));
     const result = streamText({
       model: this.model,
-      messages: newModelMessages,
+      messages: this.ModelMessages,
     });
+    let answer = '';
+    let answerSendTime = dayjs();
+    let answerCompletedTime = dayjs();
     for await (const chunk of result.textStream) {
+      if (answer === '') {
+        answerSendTime = dayjs();
+      }
+      answer += chunk;
       process.stdout.write(chunk);
     }
+    answerCompletedTime = dayjs();
+    this.messages.push(new Message({ role: 'assistant', content: answer }, answerSendTime, answerCompletedTime));
   }
 }
 
